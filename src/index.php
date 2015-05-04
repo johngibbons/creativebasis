88678767460b8d96c5cfa2e9a2af1fce19d1ee99
John Gibbons <johngibbons10@gmail.com>
2015-05-03 19:46:20 -0600

    Finish blog index styling

--- src/index.php
+++ src/index.php
@@ -5,19 +5,18 @@
 
 			<section class="page-title">
 				<h1><?php _e( 'Blog', 'html5blank' ); ?></h1>
+				<h2 class="page-subtitle"><?php _e( 'Helpful Tips and Tricks', 'html5blank' ); ?></h2>
 			</section>
 			<!-- section -->
-			<section id="blog-index">
-
-				<?php get_template_part('loop'); ?>
-
-				<?php get_template_part('pagination'); ?>
-
-			</section>
-			<!-- /section -->
 		</div>
-	</main>
+		<section id="blog-index">
 
-<?php get_sidebar(); ?>
+			<?php get_template_part('loop'); ?>
+
+			<?php get_template_part('pagination'); ?>
+
+		</section>
+			<!-- /section -->
+	</main>
 
 <?php get_footer(); ?>
--- src/loop.php
+++ src/loop.php
@@ -1,32 +1,26 @@
 <?php if (have_posts()): while (have_posts()) : the_post(); ?>
 
+	<?php if ( has_post_thumbnail()) : // Check if thumbnail exists ?>
+		<?php $url = wp_get_attachment_url( get_post_thumbnail_id($post->ID) ); ?>
+	<?php endif; ?>
+
 	<!-- article -->
 	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+		<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+		</a>
 
-		<!-- post thumbnail -->
-		<?php if ( has_post_thumbnail()) : // Check if thumbnail exists ?>
-			<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" class="post-thumb">
-				<?php the_post_thumbnail(array(600,600)); // Declare pixel size you need inside the array ?>
-				<!-- post title -->
-				<h2 class="post-title"><?php the_title(); ?></h2>
-				<!-- /post title -->
-			</a>
-		<?php endif; ?>
-		<!-- /post thumbnail -->
+		<div class="post-thumb-image" style="background-image: url('<?php echo $url ?>')"></div>
 
-		<!-- post details -->
-		<p class="date subhead">
-			<time datetime="<?php the_time('Y-m-d'); ?> <?php the_time('H:i'); ?>">
-				<?php the_date(); ?>
-			</time>
-		</p>
-		<p class="author subhead"><?php echo get_avatar( get_the_author_meta( 'ID' ), 64 ); ?> <?php the_author_posts_link(); ?></p>
-		<!-- /post details -->
-
-		<?php html5wp_excerpt('html5wp_index'); // Build your custom callback length in functions.php ?>
-
-		<?php edit_post_link(); ?>
-
+		<div class="post-thumb-details">
+			<!-- post title -->
+			<?php the_title("<h2 class='post-title'>", "</h2>"); ?>
+			<!-- /post title -->
+			<!-- post details -->
+			<p class="date subhead">
+				<time datetime="<?php the_time('Y-m-d'); ?> <?php the_time('H:i'); ?>"><?php echo get_the_date(); ?></time>
+			</p>
+			<!-- /post details -->
+		</div>
 	</article>
 	<!-- /article -->
 
